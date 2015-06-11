474d9181b353c58b3de9d1cce590daf385d16ea3
dmitry-s93 <dmitri.s93@gmail.com>
2015-06-11 23:08:56 +0300

    Restructuring and cleaning

--- www/index.php
+++ www/index.php
@@ -4,18 +4,18 @@ include_once ('include/config.php');
 $output = "
 	<link rel='stylesheet' href='template/css/map.css' />
 	<div id='map' class='map'></div>
-	<script src='template/leaflet/leaflet.js'></script>
+	<script src='template/js/leaflet.js'></script>
 	
-	<link rel='stylesheet' href='template/leaflet/plugins/leaflet.fullscreen/Control.FullScreen.css' />
-	<script src='template/leaflet/plugins/leaflet.fullscreen/Control.FullScreen.js'></script>
+	<link rel='stylesheet' href='template/css/Control.FullScreen.css' />
+	<script src='template/js/Control.FullScreen.js'></script>
 	
-	<link rel='stylesheet' href='template/leaflet/plugins/leaflet.locate/dist/L.Control.Locate.min.css' />
+	<link rel='stylesheet' href='template/css/L.Control.Locate.min.css' />
 	
 	<!--[if lt IE 9]>
-		<link rel='stylesheet' href='template/leaflet/plugins/leaflet.locate/dist/L.Control.Locate.ie.min.css'/>
+		<link rel='stylesheet' href='template/css/L.Control.Locate.ie.min.css'/>
 	<![endif]-->
 
-	<script src='template/leaflet/plugins/leaflet.locate/dist/L.Control.Locate.min.js' ></script>
+	<script src='template/js/L.Control.Locate.min.js' ></script>
 	
 	<script src='template/js/map.js'></script>
 	";
--- www/map.php
+++ www/map.php
@@ -4,7 +4,7 @@ include_once ('include/config.php');
 $dbconn = pg_connect("host=".HOST." dbname=".NAME_BD." user=".USER." password='".PASSWORD."'") or die(pg_last_error());
 
 $r_id = $_GET['id'];
-	
+
 $sql_route = pg_query("
 SELECT
 	transport_routes.tags->'route' as type,
@@ -16,24 +16,6 @@ SELECT
 FROM transport_routes
 WHERE id=".$r_id.";
 	") or die(mysql_error());
-	
-//$sql_route = pg_query("
-	//SELECT
-		//transport_routes.id,
-		//transport_routes.tags->'route' as type,
-		//transport_routes.tags->'ref' as ref,
-		//transport_routes.tags->'from' as route_from,
-		//transport_routes.tags->'via' as route_via,
-		//transport_routes.tags->'to' as route_to,
-		//transport_routes.length as length,
-		//ST_AsGeoJSON(geom) as geom
-	//FROM transport_routes, transport_location
-	//WHERE
-		//transport_location.place_id=1327509 and
-		//transport_location.route_id=transport_routes.id and
-		//transport_routes.tags->'route'='bus' and
-		//transport_routes.tags->'ref'='23'
-	//") or die(mysql_error());
 
 $sql_stops=pg_query("
 SELECT
@@ -48,7 +30,7 @@ WHERE
 	relation_members.member_id=transport_stops.id and
 	relation_members.member_role = 'stop'
 ");
-	
+
 $sql_platforms=pg_query("
 SELECT
 	transport_stops.tags->'name' as name,
@@ -69,35 +51,35 @@ $output="
 		<title>".$page_title."</title>
 			<meta name='viewport' content='width=device-width, initial-scale=1.0'>
 			<link rel='stylesheet' href='template/css/map.css' />
-			<link rel='stylesheet' href='template/leaflet/leaflet.css' />		
-			<link rel='stylesheet' href='template/font-awesome/css/font-awesome.min.css' />
-			<link rel='stylesheet' href='template/leaflet/plugins/leaflet.fullscreen/Control.FullScreen.css' />
-			<link rel='stylesheet' href='template/leaflet/plugins/leaflet.locate/dist/L.Control.Locate.min.css' />
+			<link rel='stylesheet' href='template/css/leaflet.css' />
+			<link rel='stylesheet' href='template/css/Control.FullScreen.css' />
+			<link rel='stylesheet' href='template/css/L.Control.Locate.min.css' />
+			<link rel='stylesheet' href='template/css/font-awesome.min.css' />
 			<!--[if lt IE 9]>
-				<link rel='stylesheet' href='template/leaflet/plugins/leaflet.locate/dist/L.Control.Locate.ie.min.css'/>
-			<![endif]-->		
+				<link rel='stylesheet' href='template/css/L.Control.Locate.ie.min.css'/>
+			<![endif]-->
 	</head>
 	<body style='margin: 0px'>
 
-		<div id='map' style='width: 100%; height:100%'></div>		
-		<script src='template/leaflet/leaflet.js'></script>		
-		<script src='template/leaflet/plugins/leaflet.fullscreen/Control.FullScreen.js'></script>
-		<script src='template/leaflet/plugins/leaflet.locate/dist/L.Control.Locate.min.js' ></script>
-				
+		<div id='map' style='width: 100%; height:100%'></div>
+		<script src='template/js/leaflet.js'></script>
+		<script src='template/js/Control.FullScreen.js'></script>
+		<script src='template/js/L.Control.Locate.min.js' ></script>
+
 		<script>";
 
 while ($row_route = pg_fetch_assoc($sql_route)){
-	
+
 	if ($row_route['route_from']<>'' and $row_route['route_to']<>'') {
-		$pt_name=": ".$row_route['route_from']." ⇨ ".$row_route['route_to'];	
+		$pt_name=": ".$row_route['route_from']." ⇨ ".$row_route['route_to'];
 	} else {
 		$pt_name="";
-	}		
+	}
 
 	$route_name=$transport_type_names[$row_route['type']]." ".$row_route['ref'].$pt_name;
-	
+
 	if ($row_route['geom'] != "") {
-		$output.="		
+		$output.="
 			geojsonRoute = {
 				'type': 'Feature',
 				'properties': {
@@ -107,20 +89,20 @@ while ($row_route = pg_fetch_assoc($sql_route)){
 				},
 				'geometry': ".$row_route['geom']."
 			},";
-	}		
+	}
 }
 
 $output.="
 geojsonStops = { 'type': 'FeatureCollection','features': [";
-while ($row_stops = pg_fetch_assoc($sql_stops)){	
+while ($row_stops = pg_fetch_assoc($sql_stops)){
 	if ($row_stops['geom'] != "") {
-		
+
 		//switch ($row_stops['role']) {
 			//case 'platform_entry_only': $description = "Только вход"; break;
 			//case 'platform_exit_only': $description = "Только выход"; break;
 			//default: $description = ""; break;
 		//}
-		
+
 		$output.="
 			{
 				'type': 'Feature',
@@ -131,21 +113,21 @@ while ($row_stops = pg_fetch_assoc($sql_stops)){
 				},
 				'geometry': ".$row_stops['geom']."
 			},";
-		}	
+		}
 }
-$output.="]}";		
+$output.="]}";
 
 $output.="
 geojsonPlatforms = { 'type': 'FeatureCollection','features': [";
-while ($row_platforms = pg_fetch_assoc($sql_platforms)){	
+while ($row_platforms = pg_fetch_assoc($sql_platforms)){
 	if ($row_platforms['geom'] != "") {
-		
+
 		switch ($row_platforms['role']) {
 			case 'platform_entry_only': $description = "Только вход"; break;
 			case 'platform_exit_only': $description = "Только выход"; break;
 			default: $description = ""; break;
 		}
-		
+
 		$output.="
 			{
 				'type': 'Feature',
@@ -156,12 +138,12 @@ while ($row_platforms = pg_fetch_assoc($sql_platforms)){
 				},
 				'geometry': ".$row_platforms['geom']."
 			},";
-		}	
+		}
 }
-$output.="]}";	
+$output.="]}";
 
-$output.="		
-		</script>		
+$output.="
+		</script>
 		<script src='template/js/map.js'></script>
 	</body>
 </html>";
