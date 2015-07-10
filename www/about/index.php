aa4199c773c76edcfca6438e66fd67c3caf93b17
dmitry-s93 <dmitri.s93@gmail.com>
2015-07-10 23:14:07 +0300

    Update about page

--- www/about/index.php
+++ www/about/index.php
@@ -6,21 +6,33 @@ $sql_update_date= pg_query("
 		MAX(tstamp) as tstamp
 	FROM
 		statistics_summary
-") or die(mysql_error());
+");
 
 $update_date = pg_fetch_assoc($sql_update_date)['tstamp'];
 
 $output = "
-	<h2 align=center>О сайте</h2>
-	<p>Сайт представляет собой средство просмотра и валидации маршрутов общественного транспорта международного проекта <a href='http://openstreetmap.org'>OpenStreetMap</a>.</p>
-	<p>Для просмотра доступны маршруты автобусов, троллейбусов, маршрутных такси, трамваев и поездов.</p>
-	<p>Используются данные OpenStreetMap — международного проекта по созданию свободно-распространяемой подробной карты всего мира.<br><br>
-	Веб-сайт проекта: <a href='http://openstreetmap.org'>openstreetmap.org</a><br>
-	Российский портал: <a href='http://openstreetmap.ru'>openstreetmap.ru</a><br></p>
-	<h3>Обратная связь</h3>
-	<p>Проект на <a href='https://github.com/dmitry-s93/OSMPublicTransport'>GitHub</a>.</p>
-	<h3>Техническая информация</h3>
-	<p>Дата актуализации данных: ".$update_date."</p>
+	<div class='content_center'>
+		<h2 align=center>О сайте</h2>
+		<p>
+			Данный веб-сайт представляет собой средство просмотра и валидации маршрутов общественного транспорта международного проекта <a href='http://openstreetmap.org'>OpenStreetMap</a>. Для просмотра доступны маршруты автобусов, троллейбусов, маршрутных такси, трамваев и поездов.<br>
+			<br>
+			Российский портал OSM: <a href='http://openstreetmap.ru'>openstreetmap.ru</a>
+		</p>
+		<h3>Авторские права</h3>
+		<p>
+			Проект общественного транспорта использует данные <a href='http://openstreetmap.org'>OpenStreetMap</a> — международного проекта по созданию свободно-распространяемой подробной карты всего мира.<br>
+			Данные распространяются по лицензии <a href='http://opendatacommons.org/licenses/odbl/'>Open Data Commons Open Database License</a> (ODbL).
+		</p>
+		<h3>Обратная связь</h3>
+		<p>
+			Проект на <a href='https://github.com/dmitry-s93/OSMPublicTransport'>GitHub</a>.<br>
+			Ветка на <a href='http://forum.openstreetmap.org/viewtopic.php?id=31809'>форуме</a>.
+		</p>
+		<h3>Техническая информация</h3>
+		<p>
+			Дата актуализации данных: ".$update_date."
+		</p>
+	</div>
 	";
 
 $page_title='О сайте маршрутов общественного транспорта OpenStreetMap';
