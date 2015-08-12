15d8c004b05acdc81800d8b6d3db89cf0539cb8f
vitalcrazz <pjil@yandex.ru>
2015-08-12 15:48:06 +0400

    Multiple bounding boxes

--- prepare_data/create_cache.php
+++ prepare_data/create_cache.php
@@ -1,11 +1,20 @@
 <?php
 
-$domain = 'http://osmpublictransport/';
+$domain = 'http://osmpublictransport/'; // the requested url
 
 $ZoomRange = array(14, 14); // start and finish zoom
-$LatLonStart = array("57.8613", "28.2164"); // left-top coordinates
-$LatLonFinish = array("57.7316", "28.468"); // right-bottom coordinates
 $Feature = 'platform';
+
+$bBoxes = array(
+	'Moscow' => array(
+		'start' => array(57.8613, 28.2164), // left-top coordinates
+		'finish' => array(57.7316, 28.468), // right-bottom coordinates
+	),
+	'Saint Petersburg' => array(
+		'start' => array(60.0844, 30.0909),
+		'finish' => array(59.7315, 30.6656),
+	),
+);
 
 function getTileNumber($LatLon, $zoom) {
 	$lat = $LatLon[0];
@@ -22,34 +31,37 @@ $savepath = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..') . DIRECTORY_SEPARATOR
 
 for($zoom = $ZoomRange[0]; $zoom <= $ZoomRange[1]; $zoom++) {
 
-	$start = getTileNumber($LatLonStart, $zoom);
-	$finish = getTileNumber($LatLonFinish, $zoom);
+	foreach ($bBoxes as $bKey=>$bBox) {
+		echo "Generating bbox: $bKey, zoom: $zoom...\n";
+		
+		$start = getTileNumber($bBox['start'], $zoom);
+		$finish = getTileNumber($bBox['finish'], $zoom);
 
-	for($x = $start["x"]; $x <= $finish["x"]; $x++) {
-		$dirpath = $savepath . $Feature . DIRECTORY_SEPARATOR . $zoom . DIRECTORY_SEPARATOR . $x;
-		mkdir($dirpath, 0777, TRUE);
+		for($x = $start["x"]; $x <= $finish["x"]; $x++) {
+			$dirpath = $savepath . $Feature . DIRECTORY_SEPARATOR . $zoom . DIRECTORY_SEPARATOR . $x;
+			mkdir($dirpath, 0777, TRUE);
 
-		for($y = $start["y"]; $y <= $finish["y"]; $y++) {
+			for($y = $start["y"]; $y <= $finish["y"]; $y++) {
 
-			$url = $domain . "ajax/get_json_tile.php?type=$Feature&x=$x&y=$y&z=$zoom";
-			$filepath = $dirpath . DIRECTORY_SEPARATOR . $y . ".geojson";
+				$url = $domain . "ajax/get_json_tile.php?type=$Feature&x=$x&y=$y&z=$zoom";
+				$filepath = $dirpath . DIRECTORY_SEPARATOR . $y . ".geojson";
 
-			$out = fopen($filepath,"wb");
-			if ($out == FALSE){ 
-			  print "File not opened"; 
-			  exit;
-			} 
+				$out = fopen($filepath,"wb");
+				if ($out == FALSE){ 
+					print "File not opened"; 
+					exit;
+				} 
 
-			$ch = curl_init(); 
+				$ch = curl_init(); 
 
-			curl_setopt($ch, CURLOPT_FILE, $out); 
-			curl_setopt($ch, CURLOPT_HEADER, 0); 
-			curl_setopt($ch, CURLOPT_URL, $url); 
+				curl_setopt($ch, CURLOPT_FILE, $out); 
+				curl_setopt($ch, CURLOPT_HEADER, 0); 
+				curl_setopt($ch, CURLOPT_URL, $url); 
 
-			curl_exec($ch);
+				curl_exec($ch);
 
-			curl_close($ch); 
+				curl_close($ch); 
+			}
 		}
 	}
-	
 }
