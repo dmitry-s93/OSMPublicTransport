649fec6bf0668dffbe84c7a3f057f08d06196478
dmitry-s93 <dmitri.s93@gmail.com>
2015-10-01 21:15:39 +0300

    Update scripts

--- prepare_data/restore_prepared_data.sh
+++ prepare_data/restore_prepared_data.sh
@@ -2,5 +2,5 @@
 cd $(dirname $(readlink -e $0))
 . ./config.sh
 
-pg_restore -h $db_host -d $db_name -U $db_user -t "regions" -v "BackUp/regions.backup"
-pg_restore -h $db_host -d $db_name -U $db_user -t "places" -v "BackUp/places.backup"
+pg_restore -h $db_host -d $db_name -U $db_user -t "regions" -v "osm_data/regions.backup"
+pg_restore -h $db_host -d $db_name -U $db_user -t "places" -v "osm_data/places.backup"
